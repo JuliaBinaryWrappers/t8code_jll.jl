e1cd969b1fb668955fdeda49f963727548c2a176
jlbuild <juliabuildbot@gmail.com>
2024-12-13 17:22:01 +0000

    t8code_jll build 3.0.1+0

--- src/wrappers/aarch64-apple-darwin-mpi+mpitrampoline.jl
+++ src/wrappers/aarch64-apple-darwin-mpi+mpitrampoline.jl
@@ -6,7 +6,7 @@ using MPItrampoline_jll
 JLLWrappers.@generate_wrapper_header("t8code")
 JLLWrappers.@declare_library_product(libp4est, "@rpath/libp4est.3.0.0.dylib")
 JLLWrappers.@declare_library_product(libsc, "@rpath/libsc.2.0.0.dylib")
-JLLWrappers.@declare_library_product(libt8, "@rpath/libt8.3.0.0.dylib")
+JLLWrappers.@declare_library_product(libt8, "@rpath/libt8.3.0.1.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll, MPItrampoline_jll, MPIPreferences)
     JLLWrappers.@init_library_product(
@@ -23,7 +23,7 @@ function __init__()
 
     JLLWrappers.@init_library_product(
         libt8,
-        "lib/libt8.3.0.0.dylib",
+        "lib/libt8.3.0.1.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
