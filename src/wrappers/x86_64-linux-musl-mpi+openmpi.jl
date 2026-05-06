8dd1f28938a009b40f0cab9da57738e5b5c64a61
jlbuild <juliabuildbot@gmail.com>
2026-05-06 17:42:26 +0000

    t8code_jll build 4.0.5+0

--- src/wrappers/x86_64-linux-musl-mpi+openmpi.jl
+++ src/wrappers/x86_64-linux-musl-mpi+openmpi.jl
@@ -6,7 +6,7 @@ using OpenMPI_jll
 JLLWrappers.@generate_wrapper_header("t8code")
 JLLWrappers.@declare_library_product(libp4est, "libp4est.so.3.0.0")
 JLLWrappers.@declare_library_product(libsc, "libsc.so.3.0.0")
-JLLWrappers.@declare_library_product(libt8, "libt8.so.4.0.0-26.04-dirty")
+JLLWrappers.@declare_library_product(libt8, "libt8.so.4.0.0-26.05-dirty")
 function __init__()
     JLLWrappers.@generate_init_header(Zlib_jll, OpenMPI_jll, MPIPreferences)
     JLLWrappers.@init_library_product(
